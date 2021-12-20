710b1e4df6970ebb4ed816640aea6ae0ce442935
Ali Bohlooli <bohlooli1999@gmail.com>
2021-12-20 09:20:35 +0330

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-       <Header></Header>
-      <p>contents</p>
+      <Header></Header>
+
+      {/* <Switch>
+        <Route exact path="/" component={home} />
+        <AuthRoute exact path="/login" component={login} />
+        <UserRoute exact path="/orders" component={orders} />
+        <SellerRoute exact path="/seller/orders" component={orders} />
+      </Switch> */}
+
       <Footer></Footer>
     </>
   );
