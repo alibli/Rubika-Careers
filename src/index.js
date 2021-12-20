a25676928400c18d3174f687ec368d598929c329
Parinaz Setayeshgar <p.setayeshgar@yahoo.com>
2021-12-20 13:08:13 +0330

    react-bootstrap added + unused lines cleared

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import "font-awesome/css/font-awesome.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ApplyModal from './components/ApplyModal';
-import JobDetails from './components/JobDetails';
-
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
