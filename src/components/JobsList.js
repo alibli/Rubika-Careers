15a0fb5162a632e9a99dc2bd53d9521bace930b7
ParinazSetayeshgar <p.setayeshgar@yahoo.com>
2021-12-26 21:44:02 +0330

    API sevice created + getting jobs list

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import jobsService from '../Service/JobsService';
+import apiService from "../Service/APIService";
 import '../styles/JobsList.css';
 
 function JobsList() {
@@ -8,28 +9,58 @@ function JobsList() {
     const serviceJobsList = jobsService.getJobsList();
     const [jobsList, setJobsList] = useState(serviceJobsList);
 
-    useEffect(() => {
-        // API call (setJobsList(data) & jobService.setJobLists(data) -> if data is not empty)
-    }, [])
+    // const jobsListObserver = e => {
+    //     switch (e.action) {
+    //         case 'JOBS-LIST-FILEED':
+    //             const jobsListValue = jobsService.getJobsList();
+    //             setJobsList(jobsListValue);
+    //             break;
+
+    //         default:
+    //             break;
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     jobsService.jobsListSubject.subscribe(jobsListObserver);
+
+    //     const handleResponse = apiService.handleJobsList;
+    //     apiService.getRequest('https://0.0.0.0:8000/v1/jobs', handleResponse)
+    //     // API call (setJobsList(data) & jobService.setJobLists(data) -> if data is not empty)
+
+    //     return () => {
+    //         jobsService.jobsListSubject.unsubscribe(jobsListObserver);
+    //     }
+    // }, [])
 
     return (
         <>
             <div className="jobs-list">
+                {
+                    jobsList.length !== 0
 
-                {jobsList.map(job => (
-                    <div
-                        className="job-title bg-warning rounded"
-                        key={job.id}>
-                        {job.title}
+                        ?
+                        jobsList.map(job => (
+                            <div
+                                className="job-title bg-warning rounded"
+                                key={job.id}>
+                                {job.title}
 
-                        <Link to={`/job-details/${job.id}`}>
-                            <button 
-                            className="btn">
-                                درخواست
-                            </button>
-                        </Link>
-                    </div>
-                ))}
+                                <Link to={`/job-details/${job.id}`}>
+                                    <button
+                                        className="btn">
+                                        درخواست
+                                    </button>
+                                </Link>
+                            </div>
+                        ))
+
+                        :
+                        <div className="job-title bg-warning rounded">
+                            در حال حاضر موقعیتی وجود ندارد.
+                        </div>
+
+                }
 
             </div>
         </>
