de1cd4c18351840ca8cbdb7e51e9cf607c20579a
Ali Bohlooli <bohlooli1999@gmail.com>
2021-12-27 12:15:08 +0330

    adminJobRequestDetail complete

--- src/components/AdminPanel/AdminJobRequestDetails.js
+++ src/components/AdminPanel/AdminJobRequestDetails.js
@@ -2,26 +2,54 @@ import '../../styles/AdminJobRequestDetails.css';
 
 import Table from '../Core/Table';
 import AdminFilter from './AdminFilter';
+import { Dropdown } from 'react-bootstrap';
+import { useState } from 'react';
 
 
 function AdminJobRequestDetails() {
-    
+
+    const [status,setStatus] = useState('new');
+
     const hList = [
         { id: 0, name: "نام" },
         { id: 1, name: "تاریخ درخواست" },
         { id: 2, name: "**وضعیت" },
         { id: 3, name: "جزییات" },
-        
+
     ];
 
     const rList = [
-        { id: 0, fields: ["علیرضا علی آبادی", "10/5/1400" , 'dropdown' ]},
-        { id: 1, fields: ["پریناز ستایشگر", "10/5/1400" , 'dropDown' ]},
-        { id: 2, fields: ["علی بهلولی", "10/5/1400" , 'dropDown' ]},
-        { id: 3, fields: ["فرزانه زیرک", "10/5/1400" , 'dropDown']}
+        { id: 0, fields: ["علیرضا علی آبادی", "10/5/1400"] },
+        { id: 1, fields: ["پریناز ستایشگر", "10/5/1400"] },
+        { id: 2, fields: ["علی بهلولی", "10/5/1400"] },
+        { id: 3, fields: ["فرزانه زیرک", "10/5/1400"] }
     ];
 
     const actions = [
+        {
+            caption:
+                <select 
+                className='admin-status-dropdown'
+                onChange={(e) => setStatus(e.target.value)}
+                value={status}>
+                    <option value="new" disabled>
+                        جدید
+                    </option>
+                    <option value="accepted">
+                        تایید شده
+                    </option>
+                    <option value="rejected">
+                        رد شده
+                    </option>
+                    <option value="unclear">
+                        نامشخص
+                    </option>
+                </select>
+            , onClick: (id) => {
+                // /requestdetails/$(id)
+            }
+        },
+
         {
             caption: <i className="fa fa-eye"></i>, onClick: (id) => {
                 // /requestdetails/$(id)
@@ -32,10 +60,10 @@ function AdminJobRequestDetails() {
 
     // const fList = ['نامشخص' , 'قبول' , 'رد' , 'در حال بررسی'  ];
     const fList = [
-        { id: 0 , name : 'نامشخص' },
-        { id: 1 , name : 'قبول' },
-        { id: 2 , name : 'رد' },
-        { id: 3 , name : 'در حال بررسی' },
+        { id: 0, name: 'نامشخص' },
+        { id: 1, name: 'قبول' },
+        { id: 2, name: 'رد' },
+        { id: 3, name: 'در حال بررسی' },
     ];
 
     return (
